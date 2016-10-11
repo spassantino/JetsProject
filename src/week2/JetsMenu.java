716f943b68183155d116958a5ab27c40eb6bc09c
Sarah Passantino <spassantino@yahoo.com>
2016-10-10 20:30:53 -0600

    ma to mph now working

--- src/week2/JetsMenu.java
+++ src/week2/JetsMenu.java
@@ -1,5 +1,5 @@
 package week2;
-import java.util.ArrayList;
+
 import java.lang.System;
 import java.util.*;
 
@@ -7,19 +7,21 @@ public class JetsMenu {
 	static ArrayList<Jet> fleet = new ArrayList<Jet>();
 
 	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		Jet jet1 = new Jet("a", 900, 3000000, 7000);
-		fleet.add(0,jet1);
+		fleet.add(0, jet1);
 		Jet jet2 = new Jet("b", 800, 2000000, 7600);
-		fleet.add(1,jet2);
+		fleet.add(1, jet2);
 		Jet jet3 = new Jet("c", 1000, 3240000, 7000);
-		fleet.add(2,jet3);
+		fleet.add(2, jet3);
 		Jet jet4 = new Jet("d", 990, 3200000, 6900);
-		fleet.add(3,jet4);
+		fleet.add(3, jet4);
 		Jet jet5 = new Jet("e", 1100, 40000000, 8000);
-		fleet.add(4,jet5);
+		fleet.add(4, jet5);
 		displayMenu();
 	}
+
 	public static void displayMenu() {
 
 		System.out.println("Please choose a number from the following options 1-5:");
@@ -37,40 +39,52 @@ public class JetsMenu {
 			findLongestRangeJet();
 		} else if (option == 4) {
 			addJet();
-		} else if (option == 5){
+		} else if (option == 5) {
 			quitMenu();
-		}
-		else if ((option > 6) || (option < 1)) {
+		} else if ((option > 6) || (option < 1)) {
 			System.out.println("Your input was invalid. Please only select from 1-5");
 			return;
 		}
 	}
 
 	public static void listJets() {
-//		ArrayList<Jet> fleet = new ArrayList<Jet>();
-//		Jet jet1 = new Jet("model a", 900, 3000000, 7000);
-//		fleet.add(jet1);
-//		Jet jet2 = new Jet("model b", 800, 2000000, 7600);
-//		fleet.add(jet2);
-//		Jet jet3 = new Jet("model c", 1000, 3240000, 7000);
-//		fleet.add(jet3);
-//		Jet jet4 = new Jet("model d", 990, 3200000, 6900);
-//		fleet.add(jet4);
-//		Jet jet5 = new Jet("model e", 1100, 40000000, 8000);
-//		fleet.add(jet5);
-	    for(int index=0; index < fleet.size(); index++){
-	        System.out.println(fleet.get(index));
-		 }
-		   displayMenu();
+		// ArrayList<Jet> fleet = new ArrayList<Jet>();
+		// Jet jet1 = new Jet("model a", 900, 3000000, 7000);
+		// fleet.add(jet1);
+		// Jet jet2 = new Jet("model b", 800, 2000000, 7600);
+		// fleet.add(jet2);
+		// Jet jet3 = new Jet("model c", 1000, 3240000, 7000);
+		// fleet.add(jet3);
+		// Jet jet4 = new Jet("model d", 990, 3200000, 6900);
+		// fleet.add(jet4);
+		// Jet jet5 = new Jet("model e", 1100, 40000000, 8000);
+		// fleet.add(jet5);
+		for (int index = 0; index < fleet.size(); index++) {
+			System.out.println(fleet.get(index));
 		}
-
-	public static void findFastestJet() {
-		System.out.println("The fastest Jet is: ");
 		displayMenu();
 	}
 
+	public static void findFastestJet() {
+
+			
+		System.out.println("The fastest Jet is: ");
+		displayMenu();
+	}
+	
 	public static void findLongestRangeJet() {
+		// ArrayList<Jet> minRange=new ArrayList<Jet>();
+		//
+		// for(Jet x:fleet){
+		// if(minRange.size()==0||x.getRange()==minRange.get(0).getRange())
+		// minRange.add(x);
+		// else if(x.getRange()<minRange.get(0).getRange()){
+		// minRange.clear();
+		// minRange.add(x);
+		// }
+
 		System.out.println("The longest range Jet is: ");
+		// return ;
 		displayMenu();
 	}
 
@@ -91,8 +105,7 @@ public class JetsMenu {
 		fleet.add(newJet);
 		System.out.println(newJet);
 		displayMenu();
-		}
-	
+	}
 
 	public static void quitMenu() {
 		System.out.println("You have successfully terminated the program");
